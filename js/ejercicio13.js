d796c67d294a0557571659e2c8005964bec5aa1c
Ignacio Lencina <nacholencina60@gmail.com>
2024-04-04 07:39:35 -0300

    Resolución ejercicio 13

---
+++ js/ejercicio13.js
@@ -0,0 +1,5 @@
+// 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+const cadena = "este texto mágicamente se va a reescribir en mayúsculas";
+const cadenaUpper = cadena.toUpperCase();
+console.log(cadenaUpper);
